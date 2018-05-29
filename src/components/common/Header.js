ea32f0a71ae952e824c9066c6907b9559f670769
Serge Bishyr <seriybg@gmail.com>
2018-05-29 11:05:41 +0300

    clean up header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,10 +7,10 @@ const Header = () => {
                 <div className="navbar-header">
                     <a className="navbar-brand" href="#">Dares</a>
                 </div>
-                <div className="col-lg-3 align-self-center">
+                <div className="col-lg-3">
                     <form className="navbar-form" role="search">
                         <div className="input-group">
-                            <input type="text" className="form-control" placeholder="Search" name="q" />
+                            <input type="text" className="form-control search-input" placeholder="Search" name="q" />
                             <div className="input-group-btn">
                                 <button className="btn btn-default" type="submit"><i className="fa fa-search"/></button>
                             </div>
