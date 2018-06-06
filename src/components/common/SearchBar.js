bf17997d725c28e2ff34f988565213880f3e8198
Serge Bishyr <seriybg@gmail.com>
2018-06-06 16:59:55 +0300

    fix search bar

--- src/components/common/SearchBar.js
+++ src/components/common/SearchBar.js
@@ -6,7 +6,7 @@ const SearchBar = () => {
             <form className="navbar-form" role="search">
                 <div className="input-group">
                     <input type="text" className="form-control search-input" placeholder="Search" name="q" />
-                    <div className="input-group-btn">
+                    <div className="input-group-btn search-btn">
                         <button className="btn btn-default rounded-circle" type="submit"><i className="fa fa-search"/></button>
                     </div>
                 </div>
